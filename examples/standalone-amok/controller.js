9ed12dec0b04d6f36017ce35aef45e5983ef3be0
Saulius Zukauskas <saulius.zukauskas@gmail.com>
2016-01-20 13:43:34 +0000

    updating example project with references to http mode

--- examples/standalone-amok/controller.js
+++ examples/standalone-amok/controller.js
@@ -1,6 +1,10 @@
 // example controller
 var amok = require('amokjs');
 
+// example for http amok mode
+// amok.setMode('http');
+// amok.setExternalUrl('http://httpbin.org');
+
 // example directory setup
 // amok.setResponsesDirectory('new/responses');
 
