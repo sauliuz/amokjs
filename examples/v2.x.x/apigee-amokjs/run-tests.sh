9c577c1099424cb78568d26c60244172a7047a1c
Saulius Zukauskas <saulius.zukauskas@gmail.com>
2018-05-27 19:41:33 +0100

    updating apigee example

--- examples/v2.x.x/apigee-amokjs/run-tests.sh
+++ examples/v2.x.x/apigee-amokjs/run-tests.sh
@@ -1,6 +1,4 @@
 # cucumber shoud run from within tests directory
 cd tests
-# making sure node_modules are present
-npm install
 # run tests
 ./node_modules/cucumber/bin/cucumber-js integration
---
+++ examples/v2.x.x/apigee-amokjs/tests/integration/step_definitions/amokjs-main.js
@@ -0,0 +1,23 @@
+/* jshint node:true */
+'use strict';
+
+const apickli = require('apickli');
+const {defineSupportCode} = require('cucumber');
+
+defineSupportCode(function({Before}) {
+    Before(function() {
+        this.apickli = new apickli.Apickli('http', 'apigee-75690b0a-eval-prod.apigee.net/v1/amokjs');
+        this.apickli.addRequestHeader('Cache-Control', 'no-cache');
+    });
+});
+
+//// example of adding custom step definitions
+// defineSupportCode( ({Given, When, Then}) => {
+//     Given( () => {
+//         // add custom step dafinitions for Given
+//     });
+// });
+
+// add the step definitions defined in default apickli gherkin
+// see all default dafinitions here: https://github.com/apickli/apickli#gherkin-expressions
+module.exports = require('apickli/apickli-gherkin');
